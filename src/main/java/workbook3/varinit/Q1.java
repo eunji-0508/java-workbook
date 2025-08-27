3afd35f04592517e63fe91e642fa2a695c4e1d93
eunji-0508 <cojia@naver.com>
2025-08-27 20:36:13 +0900

    feat: [varinit] Q1 구현

--- src/main/java/workbook3/varinit/Q1.java
+++ src/main/java/workbook3/varinit/Q1.java
@@ -1,18 +1,25 @@
 package main.java.workbook3.varinit;
 
+// 클래스 `InitDemo`에 두 개의 필드를 선언하고,
+// 명시적 초기화로 첫 번째 필드를 10으로, 인스턴스 초기화 블록에서 두 번째 필드를 20으로 초기화한 후 생성자에서 두 값을 더한 결과를 출력하는 프로그램을 작성
 class InitDemo {
-    int a = 10;
-    int b;
+    int a = 10;             // a를 10으로 초기화
+    int b;                  // int형 변수 b
+
+    // 인스턴스 초기화 블록
     {
-        // TODO: b 초기화
+        b = 20;             // b를 20으로 초기화
     }
+
+    // 기본 생성자
     InitDemo() {
         // TODO: a와 b의 합 출력
+        System.out.println(a + b);   // a와 b를 더한 결과를 출력함
     }
 }
 
 public class Q1 {
     public static void main(String[] args) {
-        new InitDemo();
+        new InitDemo();                             // InitDemo 객체 생성
     }
 }
