e81ce9dd79578dc295cd79e30e5fb3e3c7624612
eunji-0508 <cojia@naver.com>
2025-09-01 18:41:15 +0900

    feat: [accesslevel] Q2 구현

--- src/main/java/workbook3/accesslevel/Q2.java
+++ src/main/java/workbook3/accesslevel/Q2.java
@@ -3,22 +3,27 @@ package main.java.workbook3.accesslevel;
 import java.util.Scanner;
 
 class Student {
-    private String name;
+    private String name;        // 이름
+
     public void setName(String name) {
-        // TODO: name 설정
+        this.name = name;       // name 설정
     }
+
     public String getName() {
-        // TODO: name 반환
-        return "";
+        return name;            // name 반환
     }
 }
 
 public class Q2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n = sc.next();
-        Student s = new Student();
-        s.setName(n);
-        System.out.println(s.getName());
+        Scanner sc = new Scanner(System.in);    // 스캐너 시작
+
+        String n = sc.next();                   // 이름 입력받음
+        Student s = new Student();              // 객체 생성
+
+        s.setName(n);                           // setName() 메서드 실행
+        System.out.println(s.getName());        // getName() 메서드 실행
+
+        sc.close();                             // 스캐너 닫기
     }
 }
