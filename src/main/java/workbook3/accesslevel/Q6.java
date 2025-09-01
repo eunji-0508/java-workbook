62636e241d09660dd0feed55bfc6011fc419cc05
eunji-0508 <cojia@naver.com>
2025-09-01 18:59:07 +0900

    feat: [accesslevel] Q6 구현 보류

--- src/main/java/workbook3/accesslevel/Q6.java
+++ src/main/java/workbook3/accesslevel/Q6.java
@@ -3,12 +3,16 @@ package main.java.workbook3.accesslevel;
 import java.util.Scanner;
 
 class SecureFile {
-    private String fileName;
+    private String fileName;        // 파일 이름
+
+    // 생성자
     SecureFile(String fileName) {
         this.fileName = fileName;
     }
+
     public void read(boolean hasPermission) {
         // TODO: 권한이 있으면 파일 이름 출력, 없으면 "권한 없음" 출력
+        if(hasPermission)
     }
 }
 
